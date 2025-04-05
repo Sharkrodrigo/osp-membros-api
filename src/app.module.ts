a8557e6746840281e43c41689a852a44ea401be8
DaviRodrigo <davirodrigo2524@gmail.com>
2025-04-05 16:07:56 -0300

    ajuste para conectar

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,7 @@ import { StaticModule } from './static/static.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '12345',
-      database: 'OhOsPapo',
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true,
     }),
